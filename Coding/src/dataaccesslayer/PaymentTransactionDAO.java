18a9c56c74d986e0c97bc4dc2787d7dff923d52a
Le The Nam - HUST <63996020+LeTheNam@users.noreply.github.com>
2020-12-04 20:15:56 +0700

    Them thuoc tinh: String day; trong PaymentTransaction.java

--- Coding/src/dataaccesslayer/PaymentTransactionDAO.java
+++ Coding/src/dataaccesslayer/PaymentTransactionDAO.java
@@ -2,7 +2,7 @@ package dataaccesslayer;
 
 public class PaymentTransactionDAO {
     public static void save(int rentalCode, String cardCode, String owner, String transactionContent, int amount,
-                            int balance, String time){
+                            int balance, String time, String day){
 
     }
 }
