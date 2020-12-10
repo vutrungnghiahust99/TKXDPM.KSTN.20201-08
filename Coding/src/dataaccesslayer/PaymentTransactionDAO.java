ddc1a7d97f0f182a27963402a80f7da74668cb4a
Vu Trung Nghia 20173284 <nghia.vt173284@sis.hust.edu.vn>
2020-12-11 02:20:13 +0700

    remove balance

--- Coding/src/dataaccesslayer/PaymentTransactionDAO.java
+++ Coding/src/dataaccesslayer/PaymentTransactionDAO.java
@@ -3,16 +3,14 @@ package dataaccesslayer;
 import connection.DBConnection;
 
 public class PaymentTransactionDAO {
-    public static void save(String rentalCode, String cardCode, String owner, String transactionContent, int amount,
-                            int balance, String time, String day){
+    public static void save(String rentalCode, String cardCode, String owner, String transactionContent, int amount, String time, String day){
         String command = "INSERT INTO paymenttransaction " +
-                "(rentalCode, cardCode, owner, transactionContent, amount, balance, time, day) VALUES " +
+                "(rentalCode, cardCode, owner, transactionContent, amount, time, day) VALUES " +
                 '(' + rentalCode + ", " +
                 '\'' + cardCode + '\'' + ", " +
                 '\'' + owner + '\'' + ", " +
                 '\'' + transactionContent + '\'' + ", " +
                 amount + ", " +
-                balance + ", " +
                 '\'' + time + '\'' + ", " +
                 '\'' + day + '\'' + ')';
         DBConnection.execute(command);
