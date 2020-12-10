ff745f539d4f603a9ced702d477191d547fe803e
Vu Trung Nghia 20173284 <nghia.vt173284@sis.hust.edu.vn>
2020-12-11 02:19:44 +0700

    query with barcode

--- Coding/src/dataaccesslayer/BikeDAO.java
+++ Coding/src/dataaccesslayer/BikeDAO.java
@@ -5,20 +5,26 @@ import connection.DBConnection;
 import java.util.ArrayList;
 
 public class BikeDAO {
-    public static void updateIsInUse(boolean isInUse, int barcode, String type, int value, int priceForFirst30Minutes,
-                     int priceFor15MinutesAfter30Minutes, int remainBattery, float maxTime, String licensePlate){
+    public static void updateIsInUse(int barcode, boolean isInUse, String dockID){
         int flag;
         if (isInUse)
             flag = 1;
         else
             flag = 0;
-        String command = "UPDATE bike SET isInUse=" + flag + " WHERE barcode=" + barcode;
+        String command = "UPDATE bike SET isInUse=" + flag + ", dockID=" + '\'' + dockID + '\'' + " WHERE barcode=" + barcode;
         DBConnection.execute(command);
     }
 
     public static ArrayList<ArrayList<String>> queryWithDockID(String dockID){
         ArrayList<ArrayList<String>> s = new ArrayList<>();
         String command = "SELECT * from bike WHERE dockID=" + '\'' + dockID + '\'';
+        s = DBConnection.query(command);
+        return s;
+    }
+
+    public static ArrayList<ArrayList<String>> queryWithBarcode(int barcode){
+        ArrayList<ArrayList<String>> s = new ArrayList<>();
+        String command = "SELECT * from bike WHERE barcode=" + '\'' + barcode + '\'';
         s = DBConnection.query(command);
         return s;
     }
