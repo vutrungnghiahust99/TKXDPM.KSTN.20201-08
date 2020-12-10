0823c35cff6c95503dd987d00f2bd6dad16db631
Vu Trung Nghia 20173284 <nghia.vt173284@sis.hust.edu.vn>
2020-12-10 08:40:34 +0700

    add comments

--- Coding/src/businesslogiclayer/interbanksubsystem/IInterbank.java
+++ Coding/src/businesslogiclayer/interbanksubsystem/IInterbank.java
@@ -1,9 +1,11 @@
 package businesslogiclayer.interbanksubsystem;
 
 public interface IInterbank {
-    public abstract String processTransaction(int cost, String command, String content);
+    public abstract String processTransaction(int cost, String command, String content); // command = {"refund", "pay"}
 
-    public abstract int viewBalance();
+    public abstract int viewBalance();  //
 
     public abstract String reset();
+
+//    IInterbank s = new InterbankSubsysController();
 }
