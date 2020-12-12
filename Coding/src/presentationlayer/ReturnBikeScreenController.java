0e037bc4b39515c2bc2734ba2bf1cb8b63ec5a65
Vu Trung Nghia 20173284 <nghia.vt173284@sis.hust.edu.vn>
2020-12-13 06:45:29 +0700

    process exception when lost internet

--- Coding/src/presentationlayer/ReturnBikeScreenController.java
+++ Coding/src/presentationlayer/ReturnBikeScreenController.java
@@ -5,14 +5,19 @@ import businesslogiclayer.ReturnBikeController;
 import entities.Dock;
 import entities.RentBikeTransaction;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import presentationlayer.box.ConfirmBox;
 import presentationlayer.box.NotificationBox;
 import presentationlayer.box.NotificationBoxWithSize;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -37,16 +42,20 @@ public class ReturnBikeScreenController implements Initializable {
                 boolean confirmReturnBike = ConfirmBox.display("ConfirmBox", "Xác nhận trả xe?");
                 System.out.println(confirmReturnBike);
                 if(confirmReturnBike){
+                    System.out.println("user confirm to return bike");
                     assert dock != null;
                     newDockID = dock.getDockID();
                     RentBikeTransaction rentBikeTransaction = ReturnBikeController.processReturnBike();
-                    RentBikeController.rentalCode = "";
-                    MainScreenController.reset = true;
-
-                    System.out.println("user confirm to return bike");
-                    NotificationBoxWithSize.display("NotificationBox", rentBikeTransaction.getDetailInfo() + "\n  Trả xe thành công!", 400);
-                    Stage stage = (Stage)docksView.getScene().getWindow();
-                    stage.close();
+                    if (rentBikeTransaction == null){
+                        NotificationBox.display("Đã xảy ra lỗi", "Đã có lỗi xảy ra khi thực hiện giao dịch!");
+                    }
+                    else{
+                        RentBikeController.rentalCode = "";
+                        MainScreenController.reset = true;
+                        showRentBikeTransactionInfo(rentBikeTransaction);
+                        Stage stage = (Stage)docksView.getScene().getWindow();
+                        stage.close();
+                    }
                 }
             }
         });
@@ -69,5 +78,23 @@ public class ReturnBikeScreenController implements Initializable {
             }
         }
         return null;
+    }
+
+    public void showRentBikeTransactionInfo(RentBikeTransaction rentBikeTransaction) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("RentBikeTransactionScreen.fxml"));
+            Parent root = (Parent) loader.load();
+
+            RentBikeTransactionScreenController rentBikeTransactionScreenController = loader.getController();
+            rentBikeTransactionScreenController.initData(rentBikeTransaction);
+
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root));
+            stage.setTitle("RentBikeTransactionInfo");
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
