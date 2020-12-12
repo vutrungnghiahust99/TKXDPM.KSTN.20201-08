01d069468bab18a82a382ee2d39bea964a9a65ca
Vu Trung Nghia 20173284 <nghia.vt173284@sis.hust.edu.vn>
2020-12-13 06:46:06 +0700

    improve GUI

--- Coding/src/presentationlayer/ViewBikeScreenController.java
+++ Coding/src/presentationlayer/ViewBikeScreenController.java
@@ -1,28 +1,40 @@
 package presentationlayer;
 
+import businesslogiclayer.RentBikeController;
 import entities.Bike;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.text.Text;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class ViewBikeScreenController implements Initializable {
-    private Bike bike;
-
     @FXML
-    private Text bikeInfo;
+    private TextField text1, text2, text3, text4, text5, text6, text7, text8;
+    @FXML
+    private Button OK;
 
     @Override
-    public void initialize(URL url, ResourceBundle rb){
-        System.out.println("default initialize ViewBikeScreen");
+    public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
-    public void initData(Bike bike){
-        System.out.println("initialize bikeInfo");
-        this.bike = bike;
-        bikeInfo.setText(bike.getDetailInfo());
+    public void init(Bike bike){
+        text1.setText(Integer.toString(bike.getBarcode()));
+        text2.setText(bike.getType());
+        text3.setText(Integer.toString(bike.getValue()));
+        text4.setText(Integer.toString(bike.getPriceForFirst30Minutes()));
+        text5.setText(Integer.toString(bike.getPriceFor15MinutesAfter30Minutes()));
+        text6.setText(Integer.toString(bike.getRemainBattery()));
+        text7.setText(Float.toString(bike.getMaxTime()));
+        text8.setText(bike.getLicensePlate());
     }
 
+    public void processOKClick(){
+        System.out.println("User click on OK button");
+        Stage stage = (Stage) OK.getScene().getWindow();
+        stage.close();
+    }
 }
