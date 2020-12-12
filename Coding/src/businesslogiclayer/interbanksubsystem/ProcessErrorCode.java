03cbd28152f0a210714d278c85a5356650e0c97b
Vu Trung Nghia 20173284 <nghia.vt173284@sis.hust.edu.vn>
2020-12-13 04:46:35 +0700

    add break

--- Coding/src/businesslogiclayer/interbanksubsystem/ProcessErrorCode.java
+++ Coding/src/businesslogiclayer/interbanksubsystem/ProcessErrorCode.java
@@ -10,14 +10,14 @@ public class ProcessErrorCode {
      */
     public void process(String code){
         switch (code) {
-            case "00": System.out.println("Giao dịch thành công");
-            case "01": NotificationBox.display("Notification", "Thẻ không hợp lệ!");
-            case "02": NotificationBox.display("Notification", "Thẻ không đủ số dư!");
-            case "03": NotificationBox.display("Notification", "Internal Server Error!");
-            case "04": NotificationBox.display("Notification", "Giao dịch bị nghi ngờ gian lận!");
-            case "05": NotificationBox.display("Notification", "Không đủ thông tin giao dịch!");
-            case "06": NotificationBox.display("Notification", "Thiếu thông tin version!");
-            case "07": NotificationBox.display("Notification", "Amount không hợp lệ!");
+            case "00": System.out.println("Giao dịch thành công"); break;
+            case "01": NotificationBox.display("Notification", "Thẻ không hợp lệ!"); break;
+            case "02": NotificationBox.display("Notification", "Thẻ không đủ số dư!"); break;
+            case "03": NotificationBox.display("Notification", "Internal Server Error!"); break;
+            case "04": NotificationBox.display("Notification", "Giao dịch bị nghi ngờ gian lận!"); break;
+            case "05": NotificationBox.display("Notification", "Không đủ thông tin giao dịch!"); break;
+            case "06": NotificationBox.display("Notification", "Thiếu thông tin version!"); break;
+            case "07": NotificationBox.display("Notification", "Amount không hợp lệ!"); break;
             default: NotificationBox.display("Notification", "404 Not Found, bạn vui lòng liên hệ nhà phát hành!");
         }
     }
