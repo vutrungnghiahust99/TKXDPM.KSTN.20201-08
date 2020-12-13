2ed364ab375b4ff55f874f101d6396b0f5af9277
Vu Trung Nghia 20173284 <nghia.vt173284@sis.hust.edu.vn>
2020-12-13 08:15:44 +0700

    add comments

--- Coding/src/businesslogiclayer/InitializeController.java
+++ Coding/src/businesslogiclayer/InitializeController.java
@@ -7,7 +7,14 @@ import entities.Dock;
 
 import java.util.ArrayList;
 
+/**
+ * Khi bắt đầu chạy chương trình, giao diện chính lấy danh sách bãi xe thông qua class InitializeController
+ */
 public class InitializeController {
+    /**
+     * Lấy tất cả các bãi xe từ cơ sở dữ liệu và chuyển thành đối tượng bãi xe tương ứng
+     * @return: danh sách các bãi xe
+     */
     public static ArrayList<Dock> getDocks(){
         ArrayList<Dock> docks = new ArrayList<>();
         ArrayList<ArrayList<String>> dockTable = DockDAO.getAllDocks();
@@ -24,12 +31,22 @@ public class InitializeController {
         return docks;
     }
 
+    /**
+     * Lấy danh sách các xe trong một bãi xe cho trước từ cơ sở dữ liệu
+     * @param dockID: ID của bãi xe
+     * @return: danh sách xe
+     */
     private static ArrayList<Bike> getBikes(String dockID){
         ArrayList<Bike> bikes = new ArrayList<>();
         ArrayList<ArrayList<String>> bikeTable = BikeDAO.queryWithDockID(dockID);
         return InitializeController.tableToBikes(bikeTable);
     }
 
+    /**
+     * chuyển kết quả dạng bảng sau khi query về danh sách các xe
+     * @param bikeTable: mảng hai chiều lưu dưới dạng ArrayList<ArrayList<String>>
+     * @return: danh sách các xe
+     */
     public static ArrayList<Bike> tableToBikes(ArrayList<ArrayList<String>> bikeTable){
         ArrayList<Bike> bikes = new ArrayList<>();
         for(ArrayList<String> row: bikeTable){
