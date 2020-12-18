d67101bb1bfc5fc3e355ebd0faae91b36fc168e9
Le The Nam - HUST <63996020+LeTheNam@users.noreply.github.com>
2020-12-19 00:56:00 +0700

    unit test subsystem + rent bike controller

--- Programming/test/businesslogiclayer/controller/RentBikeControllerTest.java
+++ Programming/test/businesslogiclayer/controller/RentBikeControllerTest.java
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RentBikeControllerTest {
@@ -23,15 +28,21 @@ class RentBikeControllerTest {
     })
     void checkBarcodeAndGetBikeIfTrue(int barcode, Boolean expected) {
         Boolean isValid = RentBikeController.checkBarcodeAndGetBikeIfTrue(barcode).getKey();
-
         assertEquals(expected, isValid);
     }
 
-//    @Test
-//    void convertBikeCodeToRentalCode() {
-//    }
-//
-//    @Test
-//    void calculateDeposit() {
-//    }
+    @ParameterizedTest
+    @CsvSource({
+            "20200001, 20200001",
+            "20200010, 20200010",
+    })
+    void convertBikeCodeToRentalCode(int bikeCode, String expected) {
+        String isValid  = RentBikeController.convertBikeCodeToRentalCode(bikeCode);
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateString = df.format(date);
+        System.out.println(isValid + " " + expected + dateString);
+        assertEquals(expected+dateString, isValid);
+    }
 }
